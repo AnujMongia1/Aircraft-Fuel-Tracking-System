cfbea4a8583d64debf53f78ce865ed7b0f546f44
AnujMongia1 <amongia1308@conestogac.on.ca>
2025-04-05 15:44:01 -0400

    implemented all requirements

--- Client/Source.cpp
+++ Client/Source.cpp
@@ -1,48 +1,134 @@
 //Resources:
 //https://beej.us/guide/bgnet/html
-//https://www.geeksforgeeks.org/socket-programming-in-cpp/
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma comment(lib, "ws2_32.lib")
-#include<iostream>
+#include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
+class AircraftData {
+
+public:
+
+    int clientUniqueId;
+    string transmittedData;
+    double fuelData;
+    string dateTime;
+    string packetType;
+
+    AircraftData() {
+
+        clientUniqueId = 0;
+        transmittedData = "";
+        fuelData = 0.0;
+        dateTime = "";
+        packetType = "";
+    }
+
+    AircraftData(int id, string data, double fuel, string date_time, string type) {
+
+        clientUniqueId = id;
+        transmittedData = data;
+        fuelData = fuel;
+        dateTime = date_time;
+        packetType = type;
+
+    }
+
+    string serializePacket() {
+
+        stringstream ss;
+
+        ss << fixed << setprecision(6) << clientUniqueId << "|" << transmittedData << "|" << fuelData << "|" << dateTime << "|" << packetType;
+        return ss.str();
+
+    }
+
+    //############# SYS-050 ###############
+    static int generateUniqueClientId() {
+
+        srand(static_cast<unsigned>(time(0)));
+        return 10000 + rand() % 90000;
+
+    }
+
+};
+
+
 int main()
 {
-
     WSADATA wsaData;
-
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         fprintf(stderr, "WSAStartup failed.\n");
         exit(1);
     }
 
-    // creating socket
+    // creating a socket
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // specifying address
+    if (clientSocket < 0) {
+
+        cout << "Failed to create a socket." << endl;
+        WSACleanup();
+        return -1;
+
+    }
+
+    // specifying server address
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // sending connection request
-    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    //making connection to server
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+
+        cout << "Connection to server failed." << endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return -1;
+
+    }
+
+    //############# SYS-050 ###############
+    int uniqueId = AircraftData::generateUniqueClientId();
+    cout << "Client Unique ID: " << uniqueId << endl;
 
     // sending data
-    const char* message = "Hello, server!";
-    send(clientSocket, message, strlen(message), 0);
+    ifstream inputFile("Telem_2023_3_12 14_56_40.txt");
+    string line;
+    //############# SYS-040 - a ###############
+    while (getline(inputFile, line)) {
 
-    // to stop app from closing
+        AircraftData data(uniqueId, line, 0.0, "0", "aircraft_data");
+        //############# SYS-040 - b ###############
+        string dataPacketToSend = data.serializePacket() + "\n"; //changed to add newline
+        //############# SYS-040 - c ###############
+        send(clientSocket, dataPacketToSend.c_str(), dataPacketToSend.length(), 0);
+        cout << "ClientID:" << uniqueId << " | Data Sent: \"" << line << "\" | Packet Type: Data" << endl;
+
+    }
+
+    AircraftData lastPacket(uniqueId, "EOF", 0.0, "0", "EOF");
+
+    //############# SYS-040 - b ###############
+    string lastPacketToSend = lastPacket.serializePacket() + "\n"; //changed to add newline
+    send(clientSocket, lastPacketToSend.c_str(), lastPacketToSend.length(), 0);
+    cout << "ClientID:" << uniqueId << " | Data Sent: \"EOF\" | Packet Type: EOF" << endl;
+    
+    inputFile.close();
+    cout << "All data has been transferred!, you may close the application!";
     getchar();
-
     // closing socket
     closesocket(clientSocket);
-
     WSACleanup();
+
     return 0;
 }
--- Server/Source.cpp
+++ Server/Source.cpp
@@ -1,22 +1,267 @@
 //Resources:
 //https://beej.us/guide/bgnet/html
-//https://www.geeksforgeeks.org/socket-programming-in-cpp/
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #pragma comment(lib, "ws2_32.lib")
-#include<iostream>
+#include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <thread>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
+class AircraftData{
+
+public:
+
+    int clientUniqueId;
+    string transmittedData;
+    double fuelData;
+    string dateTime;
+    string packetType;
+    static double prevFuelReading;
+
+    AircraftData() {
+
+        clientUniqueId = 0;
+        transmittedData = "";
+        fuelData = 0.0;
+        dateTime = "";
+        packetType = "";
+    }
+
+    AircraftData(int id, string data, double fuel, string date_time, string type) {
+
+        clientUniqueId = id;
+        transmittedData = data;
+        fuelData = fuel;
+        dateTime = date_time;
+        packetType = type;
+    }
+
+     //############# SYS-010 - b ###############
+     static AircraftData deserialize(string& data) {
+
+         AircraftData ad;
+         stringstream ss(data);
+         string item;
+
+         vector<string> items;
+
+         while (getline(ss, item, '|')) {
+             items.push_back(item);
+         }
+
+         if (items.size() >= 5) {
+
+             ad.clientUniqueId = stoi(items[0]);
+             ad.transmittedData = items[1];
+             ad.fuelData = stod(items[2]);
+             ad.dateTime = items[3];
+             ad.packetType = items[4];
+         }
+
+         return ad;
+     }
+
+     //https://stackoverflow.com/questions/216823/how-to-trim-a-stdstring/20262860#20262860
+     std::string trim(std::string str)
+     {
+         size_t end = str.find_last_not_of(" \n\r\t");
+         if (end != string::npos)
+             str.resize(end + 1);
+
+         size_t start = str.find_first_not_of(" \n\r\t");
+         if (start != string::npos)
+             str = str.substr(start);
+
+         return str;
+     }
+
+
+     //############# SYS-010 - b ###############
+     void parseData() {
+         // Trim whitespace from the transmitted data
+         string dataLine = trim(transmittedData);
+
+         vector<string> items;
+         stringstream ss(dataLine);
+         string item;
+
+         while (getline(ss, item, ',')) {
+
+             item = trim(item);
+             items.push_back(item);
+
+
+         }
+
+         // saving fuel data to a file named with ClientUniqueId
+         string filename = to_string(clientUniqueId) + ".txt";
+         ofstream outfile;
+         double consumption = 0.0;
+
+         //parsing first line
+         if (dataLine.find("FUEL TOTAL QUANTITY") != string::npos) {
+
+             if (items.size() == 3) {
+
+                 dateTime = items[1];
+                 fuelData = stod(items[2]);
+
+             }
+
+             consumption = 0.0;
+             AircraftData::prevFuelReading = fuelData;
+             outfile.open(filename, ios::out);
+
+         }
+
+
+         //parsing all other lines
+         else {
+            
+             if (items.size() == 2) {
+
+                 dateTime = items[0];
+                 fuelData = stod(items[1]);
+
+             }
+
+             consumption = AircraftData::prevFuelReading - fuelData;
+             AircraftData::prevFuelReading = fuelData;
+             outfile.open(filename, ios::app);
+
+         }
+
+         //############# SYS-010 - c ###############
+         if (outfile.is_open()) {
+
+             if (fuelData != 0.000000) {
+
+                 outfile << fixed << setprecision(6) << consumption << "\n";
+                 outfile.close();
+             }
+         }
+
+         else {
+             cerr << "File could not be openend: " << filename << endl;
+
+         }
+     }
+
+};
+
+double AircraftData::prevFuelReading = 0.0;
+
+
+void calculateAverageFuelConsumption(int clientUniqueId) {
+
+    string fileName = to_string(clientUniqueId) + ".txt";
+    ifstream inputFile(fileName);
+
+    if (!inputFile.is_open()) {
+
+        cerr << "File could not be opened: " << fileName << ".txt" << endl;
+
+    }
+
+    double sum = 0.0;
+    int count = 0;
+    double value;
+
+    while (inputFile >> value) {
+        sum += value;
+        count++;
+    }
+
+    inputFile.close();
+
+    if (count > 0) {
+
+        double average = sum / count;
+
+        string fileName = to_string(clientUniqueId) + ".txt";
+        ofstream outfile(fileName, ios::app);
+
+        cout << "Average Fuel Consumption for flight " << clientUniqueId << " is: "<< fixed << setprecision(6) << average << endl;
+
+        if (outfile.is_open()) {
+
+            outfile << "Average:" << fixed << setprecision(6) << average << "\n";
+            outfile.close();
+
+        }
+    }
+
+}
+
+
+void processPacketLines(string& buffer) {
+
+    size_t pos = 0;
+
+    while ((pos = buffer.find('\n')) != string::npos) {
+
+        string packet = buffer.substr(0, pos);
+
+        buffer.erase(0, pos + 1);
+
+        if (packet.empty()) {
+            continue;
+        }
+
+        AircraftData ad = AircraftData::deserialize(packet);
+
+        if (ad.packetType == "EOF") {
+
+            cout << "ClientID:" << ad.clientUniqueId << " | Data Received: \"" << ad.transmittedData << "\" | Packet Type: EOF" << endl;
+            cout << "All data from Client has been received." << endl;
+            calculateAverageFuelConsumption(ad.clientUniqueId);
+            return;
+        }
+
+        else {
+            cout << "ClientID:" << ad.clientUniqueId << " | Data Received: \"" << ad.transmittedData << "\" | Packet Type: Data" << endl;
+        }
+
+        //############# SYS-010 - b ###############
+        ad.parseData();
+
+    }
+}
+
+
+//handling client connection (receiving data)
 void handleConnection(int clientSocket) {
 
-    //printing message from clients
-    char buffer[1024] = { 0 };
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Mesasage from Client: " << buffer << endl;
+
+    char recvBuffer[1024] = { 0 };
+    string lineBuffer;
+
+    while (true) {
+        int bytesReceived = recv(clientSocket, recvBuffer, sizeof(recvBuffer) - 1, 0);
+
+        if (bytesReceived <= 0) {
+            break;
+        }
+
+        lineBuffer.append(recvBuffer, bytesReceived);
+
+        processPacketLines(lineBuffer);
+        
+        if (lineBuffer.find("EOF") != string::npos) {
+            cout << "All data from Client has been received." << endl;
+            break;
+        }
+    }
+
+    closesocket(clientSocket);
 
 }
 
@@ -41,14 +286,15 @@ int main() {
     //binding socket to address
     bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
-    //listening for unlimited number of connections (SYS-001)
+    //listening for unlimited number of connections
     listen(serverSocket, INT_MAX);
 
+    //############# SYS-001 ###############
     while (true) {
+
         int clientSocket = accept(serverSocket, nullptr, nullptr);
         thread newThread(handleConnection, clientSocket);
         newThread.detach();
-
     }
 
     // to stop app from closing
@@ -57,5 +303,6 @@ int main() {
     // closing socket
     closesocket(serverSocket);
     WSACleanup();
+    return 0;
 
 }
